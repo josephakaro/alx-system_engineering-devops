c47aba3448580d09c314ea5699a700b4a8b46481
josephmarko <josephmmakaro@gmail.com>
2023-10-31 14:52:29 -0700

    Textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,17 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(//).join
 
+file = "text_messages.log"
+
+File.open(file, "r") do |f|
+  f.each_line do |line|
+    match = line.match(/\[(.*?)\],\[(.*?)\]\(([^)]*)\)/
+
+    if match
+      sender = match[1]
+      receiver = match[2]
+      flags = match[3]
+
+      puts "[#{sender}],[#{receiver}],[#{flags}]"
+    end
+  end
+end
