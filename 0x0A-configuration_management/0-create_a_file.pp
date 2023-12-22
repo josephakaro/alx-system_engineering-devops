496620fa42060feb38ad19f134b3c3f8736f59fc
Joseph Marko <josephmmakaro@gmail.com>
2023-12-22 23:27:30 +0000

    Reconfigured manifest😎

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,16 @@
-file{'0-create_a_file':
-  ensure => 'file',
-  path => '/tmp/school',
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
+# ... Create a file in '\tmp' directory with the following requirements:
+# - Filename: school
+# - File content: I love Puppet
+# - File permissions: 0744
+# - File owner: www-data
+# - File group: www-data
+# - File location: /tmp/school
+
+file{ '0-create_a_file':
+  ensure  => 'file',
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet',
 }
