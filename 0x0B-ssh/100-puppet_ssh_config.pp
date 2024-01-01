57ab56afe1ca8d3bc5afd023225cd277a0aabc50
Joseph Marko <josephmmakaro@gmail.com>
2024-01-01 10:18:07 +0000

    ssh: Done

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,12 @@
-file{ '/home/joseph/.ssh/config':
-    ensure  => file,
-    owner   => 'ubuntu'
-    group   => 'ubuntu'
-    mode    => '0600',
-    content => '
+# Puppet script to create ssh config file
+file_line { 'No Auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'identity':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
 }
