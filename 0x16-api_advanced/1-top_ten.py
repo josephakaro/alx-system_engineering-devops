637335aedb9e955b68ad2eacf4945fec6e9f18c6
Joseph Akaro <josephmmakaro@gmail.com>
2024-06-06 11:52:48 +0300

    pycodestyle 1-top-ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Python script that fetches the top ten hot posts from a given subreddit through the Reddit API.
+Python script that fetches the top ten
+hot posts from a given subreddit through the Reddit API.
 """
 
 import requests
@@ -9,25 +10,28 @@ import requests
 def top_ten(subreddit):
     """
     Prints the titles of the first 10 hot posts listed for a given subreddit.
-    
+
     Args:
         subreddit (str): The name of the subreddit.
     """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {"User-Agent": "My-User-Agent"}
-    params = {"limit": 9}
-    
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-    
+    params = {"limit": 10}
+
+    response = requests.get(url,
+                            headers=headers,
+                            params=params,
+                            allow_redirects=False)
+
     if response.status_code != 200:
         print("None")
         return
-    
+
     data = response.json().get("data", {}).get("children", [])
     if not data:
         print("None")
         return
-    
+
     for post in data:
         title = post.get("data", {}).get("title", "None")
         print(title)
