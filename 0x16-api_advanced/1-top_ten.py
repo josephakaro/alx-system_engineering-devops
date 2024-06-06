a982d11cf6f57c896efd532241e6dcdd4921f472
Joseph Akaro <josephmmakaro@gmail.com>
2024-06-06 12:02:40 +0300

    pycodestyle top-ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,30 +10,33 @@ import requests
 def top_ten(subreddit):
     """
     Prints the titles of the first 10 hot posts listed for a given subreddit.
-    
+
     Args:
         subreddit (str): The name of the subreddit.
     """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {"User-Agent": "My-User-Agent"}
     params = {"limit": 10}
-    
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-    
+
+    response = requests.get(url,
+                            headers=headers,
+                            params=params,
+                            allow_redirects=False)
+
     if response.status_code != 200:
         print("None")
         return
-    
+
     try:
         data = response.json().get("data", {}).get("children", [])
     except ValueError:
         print("None")
         return
-    
+
     if not data:
         print("None")
         return
-    
+
     for post in data:
         title = post.get("data", {}).get("title", "None")
         print(title)
