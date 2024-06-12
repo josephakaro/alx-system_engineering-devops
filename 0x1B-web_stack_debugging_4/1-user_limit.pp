25ff4315da74ab978c3ba2c46d43da09f306bd7d
Joseph Akaro <josephmmakaro@gmail.com>
2024-06-12 19:01:53 +0300

    Web stack Debugging #4 is Done

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -12,4 +12,4 @@ exec { 'increase-soft-file-limit-for-holberton-user':
   command	=> 'sed -i "s/holberton soft nofile [0-9]*/holberton soft nofile 50000/" /etc/security/limits.conf',
   path		=> ['/usr/local/bin', '/bin'],
   unless	=> 'grep -q "holberton soft nofile 50000" /etc/security/limits.conf', # Prevents the command from running if the limit is already set
-}`
+}
